f703e23370133458027518b1774268709752a839
CarlosEduardoValladares <carloseduardovdm@gmail.com>
2018-09-19 16:16:04 -0300

    Inicío da parte de Carregamento e Exibição das informações de um livro

---
+++ Alexandr_IA/Livro/detalhesLivro.php
@@ -0,0 +1,27 @@
+<html>
+
+	<head>
+	
+		<title>Detalhes do Livro</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="../ArquivosStyle/FolhaDeEstilo.css">
+		
+		<style>
+			
+			body{
+				
+				margin-top: 5%;
+				
+			}
+		
+		</style>
+	
+	</head>
+	
+	<body>
+	
+		<h1>Biblioteca CPII - Caxias</h1>
+	
+	</body>
+
+</html>
