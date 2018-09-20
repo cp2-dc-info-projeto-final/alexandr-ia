0e10242039be122d8a09d4739e9bd429222d8d1f
CarlosEduardoValladares <carloseduardovdm@gmail.com>
2018-09-20 15:37:58 -0300

    Novas funções de pesquisa adcionadas

--- Alexandr_IA/Modelo/TabelaLivros.php
+++ Alexandr_IA/Modelo/TabelaLivros.php
@@ -27,4 +27,54 @@
 		$sql -> execute();
 		
 	}
+	
+	function DetalhaLivro($id){
+		
+		$bd = CriaConexãoBd();
+		
+		$sql = $bd -> prepare('SELECT * FROM livro WHERE id = :id');
+		$sql -> bindValue(':id', $id);
+		
+		$sql -> execute();
+		$sql = $sql -> fetch();
+		
+		return($sql);
+		
+	}
+	
+	function PesquisaLivro($string, $tipo){
+		
+		$bd = CriaConexãoBd();
+		
+		if($tipo == 'pp_titulo'){ // pp_ é Pesquisar Por
+		
+			$sql = $bd -> prepare('SELECT titulo, autor FROM livro WHERE titulo LIKE %:string%');
+			$sql -> bindValue(':string', $string);
+			
+			$sql -> execute();
+			$sql = $sql -> fetch();
+			
+		}
+		
+		if($tipo == 'pp_autor'){
+		
+			$sql = $bd -> prepare('SELECT titulo, autor FROM livro WHERE autor LIKE %:string%');
+			$sql -> bindValue(':string', $string);
+			
+			$sql -> execute();
+			$sql = $sql -> fetch();
+			
+		}
+		
+		if($tipo == 'pp_editora'){
+		
+			$sql = $bd -> prepare('SELECT titulo, autor FROM livro WHERE editora LIKE %:string%');
+			$sql -> bindValue(':string', $string);
+			
+			$sql -> execute();
+			$sql = $sql -> fetch();
+			
+		}
+		
+	}
 ?>
