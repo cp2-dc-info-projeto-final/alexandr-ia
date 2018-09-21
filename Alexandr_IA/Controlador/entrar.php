9945c3f17d44ec0036658162d8799d7c2a83f0e5
CarlosEduardoValladares <carloseduardovdm@gmial.com>
2018-09-21 15:24:51 -0300

    Adicionando o arquivo de pagina inicial e redirecionamento incompletos

--- Alexandr_IA/Controlador/entrar.php
+++ Alexandr_IA/Controlador/entrar.php
@@ -12,23 +12,23 @@
 
 	$email = $request['email'];
 	$senha = $request['senha'];
-	
+
 	$bd = CriaConexãoBd();
 	$sql_1 = $bd -> prepare('SELECT email FROM usuario WHERE email = :email');
 	$sql_1 -> bindValue(':email', $email);
 	$sql_1 -> execute();
-	
+
 	$sql_2 = $bd -> prepare('SELECT senha FROM usuario WHERE email = :email');
 	$sql_2 -> bindValue(':email', $email);
 	$sql_2 -> execute();
-	
+
 	$sql_2 = $sql_2 -> fetch();
 	$hash = $sql_2['senha'];
 
 	if ($email == false)
 	{
 		$erro[] = "E-Mail não informado";
-	}	
+	}
 	else if ($sql_1 -> rowCount() == 0)
 	{
 		$erro[] = "Nenhum cliente encontrado para este E-Mail";
@@ -37,7 +37,7 @@
 	{
 		$erro[] = "Senha não informada";
 	}
-	
+
 	else if (password_verify($senha, $hash)){
 
 		session_start();
@@ -56,6 +56,6 @@
 	session_start();
 
 	$_SESSION['erro'] = $erro;
-	header('Location: ../LoginAluno/paginaLogin.php');
+	header('Location: ../PaginaInicial/PI_aluno_prof.php');
 
 ?>
---
+++ Alexandr_IA/PaginaInicial/PI_aluno_prof.php
@@ -0,0 +1,57 @@
+<html>
+      <head>
+        <link rel="stylesheet" type="text/css" href="../ArquivosStyle/FolhaDeEstilo.css">
+          <style>
+
+                h1{
+                  margin-bottom: -50px;
+                }
+                ul {
+                    list-style-type: none;
+                    margin: 0;
+                    padding: 0;
+                    overflow: hidden;
+                    background-color: #000000;
+                }
+                li {
+                    float: left;
+                    padding-right: 4%;
+                    padding-left: 4%;
+                }
+                li a {
+                    display: block;
+                    color: white;
+                    text-align: center;
+                    padding: 14px 16px;
+                    text-decoration: none;
+                }
+                li a:hover {
+                    background-color: #32909a;
+                }
+              .cordetela {
+                  background-color: #32909a;
+              }
+
+          </style>
+
+        <title> Página Inicial </title>
+        <meta charset="utf-8">
+
+      </head>
+      <body>
+
+        <h1>Biblioteca CPII - Caxias</h1>
+        <br><br><br><br>
+        <div>
+          <ul>
+              <li class="cordetela"><a href="PI_aluno_prof.php"> Página Inicial </a></li>
+              <li><a href="PI_aluno_prof.php"> Lista de Livros </a></li>
+              <li><a href="PI_aluno_prof.php"> Perfil </a></li>
+              <li style="float:right"><a class="cordetela" href="../Controlador/sair.php">Sair</a></li>
+          </ul>
+
+
+
+      </body>
+
+</html>
