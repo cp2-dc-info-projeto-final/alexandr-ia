fc9f2d1456ecd41f2a9d77f72b31aab867b379c0
CarlosEduardoValladares <carloseduardovdm@gmial.com>
2018-09-25 10:06:15 -0300

    Adcionada página de listagem de livros

---
+++ Alexandr_IA/Livro/listagemDeLivros.php
@@ -0,0 +1,35 @@
+<html>
+
+	<head>
+	
+		<title>Lista de Livros</title>
+		<meta charset="utf-8">
+	
+	</head>
+	
+	<body>
+	
+		<h1>Biblioteca CPII - Caxias</h1>
+		
+		<div id="barra">
+
+			<div id="itens_nav">
+				<a href="PI_aluno_prof.php">Página Inicial</a>			
+				<a href="../Livro/listagemDeLivros.php">Lista de Livros</a>			
+				<a href="">Perfil</a>
+			</div>
+			<div id="direita">
+				<div id="pesquisa">
+					<input type="text" placeholder="Pesquisa...">
+					<input type="submit">
+				</div>
+				<a id = "sair" href="../Controlador/sair.php">Sair</a>
+			</div>
+		
+		</div>
+        <br><br>
+
+	
+	</body>
+
+</html>
